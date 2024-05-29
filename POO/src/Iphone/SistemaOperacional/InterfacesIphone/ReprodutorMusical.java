f0b1c1c9ead81faaa8a0e47462c1bf2e70281df8
Marina Pires <marinapiressg@gmail.com>
2024-05-29 18:23:48 -0300

    Desafio 3 - POO

---
+++ POO/src/Iphone/SistemaOperacional/InterfacesIphone/ReprodutorMusical.java
@@ -0,0 +1,13 @@
+package Iphone.SistemaOperacional.InterfacesIphone;
+
+public interface ReprodutorMusical {
+
+    void selecionarMusica(String musica);
+
+    void tocarMusica();
+
+    void pausarMusica();
+
+    void menuReprodutorMusical();
+
+}
